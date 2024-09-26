2c30081e55fa0e6b973afc2052b81c0ca9609e85
prabhsuratsingh <prabhsurat17@gmail.com>
2024-09-26 21:49:48 +0530

    modified settings.py

--- DjangoChat/settings.py
+++ DjangoChat/settings.py
@@ -36,7 +36,11 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+
+    'daphne',
     'django.contrib.staticfiles',
+
+    'channels'
 ]
 
 MIDDLEWARE = [
@@ -121,3 +125,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ASGI_APPLICATION = 'DjangoChat.asgi.application'
